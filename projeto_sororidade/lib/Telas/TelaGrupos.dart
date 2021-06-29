dfb4661d4817608bc046163146dc890ba4cdc10f
Vitor <vitordearec@gmail.com>
2021-06-29 15:45:27 -0300

    Atualizações
    Atualizações de bibliotecas e do widget RaisedButton, para ElevatedButton

--- projeto_sororidade/lib/Telas/TelaGrupos.dart
+++ projeto_sororidade/lib/Telas/TelaGrupos.dart
@@ -5,27 +5,25 @@ class TelaGrupos extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Todos os grupos"),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          RaisedButton(
-            child: Text("Voltar"),
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          ),
-
-          RaisedButton(
-              child: Text('Conversa'),
-              onPressed: () {
-                Navigator.push(context,  MaterialPageRoute(builder: (context) => TelaConversa()));
-              },
-
-          ),
-
-    ]));
+        appBar: AppBar(
+          title: Text("Todos os grupos"),
+        ),
+        body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ElevatedButton(
+                child: Text("Voltar"),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ElevatedButton(
+                child: Text('Conversa'),
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => TelaConversa()));
+                },
+              ),
+            ]));
   }
 }
